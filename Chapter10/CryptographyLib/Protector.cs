ebdad9cdf8bedcd75fd1dc225cbff47b68345f9f
Mark J Price <markjprice@gmail.com>
2020-11-27 10:56:19 +0000

    Increased iterations for PBKDF2

--- Chapter10/CryptographyLib/Protector.cs
+++ Chapter10/CryptographyLib/Protector.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using System.Xml.Linq;
 using static System.Convert;
+using static System.Console;
 
 namespace Packt.Shared
 {
@@ -15,8 +17,10 @@ namespace Packt.Shared
     private static readonly byte[] salt =
       Encoding.Unicode.GetBytes("7BANANAS");
 
-    // iterations must be at least 1000, we will use 2000 
-    private static readonly int iterations = 2000;
+    // iterations should be high enough to take at least 100ms to 
+    // generate a Key and IV on the target machine. 50,000 iterations
+    // takes 131ms on my 3.3 GHz Dual-Core Intel Core i7 MacBook Pro.
+    private static readonly int iterations = 50_000;
 
     public static string Encrypt(
       string plainText, string password)
@@ -26,11 +30,17 @@ namespace Packt.Shared
 
       var aes = Aes.Create(); // abstract class factory method
 
+      var stopwatch = Stopwatch.StartNew();
+
       var pbkdf2 = new Rfc2898DeriveBytes(
         password, salt, iterations);
 
       aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key 
       aes.IV = pbkdf2.GetBytes(16); // set a 128-bit IV 
+
+      WriteLine("{0:N0} milliseconds to generate Key and IV using {1:N0} iterations.",
+        arg0: stopwatch.ElapsedMilliseconds,
+        arg1: iterations);
 
       using (var ms = new MemoryStream())
       {
